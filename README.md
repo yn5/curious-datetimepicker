66f99f4361011ce0fab4059ac3e0480635663883
yn5 <ynsnetmail@gmail.com>
2015-05-09 15:52:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# DateTime Picker by Curious Solutions
+Responsive flat design [jQeury DateTime Picker](http://curioussolutions.github.io/DateTimePicker) plugin for Web & Mobile
+
+### Installation
+
+```bash
+meteor add yn5:curious-datetimepicker
+```
---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'yn5:curious-datetimepicker',
+  version: '0.1.7',
+  summary: 'Responsive flat design jQuery DateTime Picker plugin for Web & Mobile',
+  git: 'https://github.com/yn5/curious-datetimepicker',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.4.2');
+  api.use('jquery');
+  api.addFiles('DateTimePicker.min.css', 'client');
+  api.addFiles('DateTimePicker.min.js', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.addFiles('DateTimePicker.min.css');
+  api.addFiles('DateTimePicker.min.js');
+});
